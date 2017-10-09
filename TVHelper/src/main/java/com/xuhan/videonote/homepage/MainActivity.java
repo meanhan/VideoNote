74b3e0248d7e05faa428dc07790d80f34411916b
xu.han <xu.han@soaros.com>
2017-10-09 17:58:31 +0800

    实现BottomNavigationBar,以及页面切换.

--- TVHelper/src/main/java/com/xuhan/videonote/homepage/MainActivity.java
+++ TVHelper/src/main/java/com/xuhan/videonote/homepage/MainActivity.java
@@ -1,17 +1,36 @@
 package com.xuhan.videonote.homepage;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.ashokvarma.bottomnavigation.BadgeItem;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.xuhan.videonote.R;
+import com.xuhan.videonote.adapter.ViewPagerAdapter;
+import com.xuhan.videonote.discover.DiscoverFragment;
+import com.xuhan.videonote.list.ListFragment;
+import com.xuhan.videonote.me.MeFragment;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener,
+        ListFragment.OnFragmentInteractionListener, DiscoverFragment.OnFragmentInteractionListener,
+        MeFragment.OnFragmentInteractionListener, BottomNavigationBar.OnTabSelectedListener {
+
+    private BottomNavigationBar mNavigationBar;
+    private BadgeItem badgeItem; //添加角标
+    private ViewPager mViewPager;
+    private ArrayList<Fragment> mFragmentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        initViewPager();
+        initNavigationBar();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +70,70 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initViewPager() {
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mFragmentList = new ArrayList<>();
+        mFragmentList.add(HomeFragment.newInstance("", ""));
+        mFragmentList.add(ListFragment.newInstance("", ""));
+        mFragmentList.add(DiscoverFragment.newInstance("", ""));
+        mFragmentList.add(MeFragment.newInstance("", ""));
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), mFragmentList);
+        mViewPager.setAdapter(adapter);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mNavigationBar.selectTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void initNavigationBar() {
+        mNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        mNavigationBar.setTabSelectedListener(this);
+        mNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        mNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+        mNavigationBar.setBarBackgroundColor(R.color.colorWhite);
+        mNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp, "首页").setActiveColorResource(
+                R.color.colorGreen))
+                .addItem(new BottomNavigationItem(R.drawable.ic_book_white_24dp, "列表").setActiveColorResource(
+                        R.color.colorGreen))
+                .addItem(new BottomNavigationItem(R.drawable.ic_find_replace_white_24dp, "发现").setActiveColorResource(
+                        R.color.colorGreen))
+                .addItem(new BottomNavigationItem(R.drawable.ic_favorite_white_24dp, "我的").setActiveColorResource(
+                        R.color.colorGreen))
+                .setFirstSelectedPosition(0)
+                .initialise();
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+        mViewPager.setCurrentItem(position);
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
