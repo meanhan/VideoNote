65604dfc32e9e004933213a0428b4618b84e3615
xu.han <xu.han@soaros.com>
2017-10-10 09:56:06 +0800

    实现BottomNavigationBar,以及页面切换.

---
+++ TVHelper/src/main/java/com/xuhan/videonote/adapter/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.xuhan.videonote.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * 底部导航对应的ViewPager的适配器
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mFragmentList;
+
+    public ViewPagerAdapter(FragmentManager fm, List<Fragment> mFragmentList) {
+        super(fm);
+        this.mFragmentList = mFragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
---
+++ TVHelper/src/main/java/com/xuhan/videonote/homepage/HomePresenter.java
@@ -0,0 +1,30 @@
+package com.xuhan.videonote.homepage;
+
+import com.xuhan.videonote.mvp.HomeContract;
+
+/**
+ * Created by meanhan on 2017/5/9.
+ */
+
+public class HomePresenter implements HomeContract.Presenter {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loadPosts() {
+
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+
+    @Override
+    public void loadMore() {
+
+    }
+}
