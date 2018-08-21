94d08c16763b013e19c53b9175b8cefe74fe851f
xu.han <xu.han@soaros.com>
2018-08-21 15:34:28 +0800

    加入LeakCanary

--- TVHelper/src/main/java/com/xuhan/videonote/application/HomeApplication.java
+++ TVHelper/src/main/java/com/xuhan/videonote/application/HomeApplication.java
@@ -1,16 +1,22 @@
 package com.xuhan.videonote.application;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.bilibili.boxing.BoxingMediaLoader;
 import com.bilibili.boxing.loader.IBoxingMediaLoader;
+import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 import com.xuhan.videonote.moment.boxing.BoxingGlideLoader;
 
 /**
- * @author  meanhan on 2017/5/6.
+ * @author meanhan on 2017/5/6.
  */
 
 public class HomeApplication extends Application {
+
+    private RefWatcher refWatcher;
 
     public HomeApplication() {
     }
@@ -22,5 +28,22 @@ public class HomeApplication extends Application {
         // 初始化 boxing选择器
         IBoxingMediaLoader loader = new BoxingGlideLoader();
         BoxingMediaLoader.getInstance().init(loader);
+
+        // 初始化Facebook Stetho
+        Stetho.initializeWithDefaults(this);
+
+        //初始化LeakCanary
+        refWatcher = LeakCanary.install(this);
+    }
+
+    /**
+     * 提供获取RefWatcher方法,可用于在Fragment中监控
+     *
+     * @param context
+     * @return RefWatcher
+     */
+    public static RefWatcher getRefWatcher(Context context) {
+        HomeApplication application = (HomeApplication) context.getApplicationContext();
+        return application.refWatcher;
     }
 }
